818dd53f83a15b6a9f1aff9362a257e4a01e478f
AksaZaheer <aqsazaheer982@gmail.com>
2024-12-14 15:55:55 +0500

    Add files via upload

---
+++ Teacher_portal.cpp
@@ -0,0 +1,11 @@
+        //Teacher portal(CMS,LMS)
+
+        //including libraries
+#include <iostream>
+#include <string>
+#include <vector>
+
+        //using standard namespace
+using namespace std;
+
+
